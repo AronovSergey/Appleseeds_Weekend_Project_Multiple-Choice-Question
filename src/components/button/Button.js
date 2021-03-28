a0c25c575a69cd1719f630679f48ea8866e78b1c
Sergey <aronovsergeyy@gmail.com>
2021-03-28 23:50:19 +0300

    add result stage

--- src/components/button/Button.js
+++ src/components/button/Button.js
@@ -1,16 +1,17 @@
 import React from 'react'
 
-const Button = ({ text, onClick, type, selectedQuestion }) => {
+const Button = ({ text, onClick, type, selectedQuestion, disabled = false }) => {
     const isSubmit = type === "next" && selectedQuestion === 9;
     return (
         <button 
-            className={`btn ${(type === "next"&& selectedQuestion === 9) ? "secondary" : ""}`} 
+            className={`btn ${(disabled && isSubmit) ? "secondary" : ""}`} 
             style={{
                 left: type === "previous" ? "10%" : "",
                 right: type === "next" ? "10%" : "",
                 display: (type === "previous" && selectedQuestion === 0) ? "none" : "",
             }}
             onClick={onClick}
+            disabled={disabled && isSubmit}
         >
             {isSubmit ? "Submit" : text}
         </button>
--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -8,6 +8,7 @@ export default class Main extends Component {
     state = {
         selectedQuestion: 0,
         data: [],
+        displayResult: false,
     }
 
     componentDidMount() {
@@ -21,6 +22,9 @@ export default class Main extends Component {
     onNextClick() {
         if(this.state.selectedQuestion < dataLength - 1)
             this.setState({ selectedQuestion: this.state.selectedQuestion + 1 })
+
+        else if(this.state.selectedQuestion === dataLength - 1)
+            this.setState({ displayResult: true })
     }
 
     onPreviousClick() {
@@ -37,36 +41,48 @@ export default class Main extends Component {
     render() {
         if(this.state.data.length > 0) {
             const { question, answers, userChoose } = this.state.data[this.state.selectedQuestion];
-            return (
-                <div>
-                    <QuestionsNavbar 
-                        answers={this.state.data.map(question => question.userChoose)}
-                        onClick={(index) => this.onNavbarClick(index)}
-                        selectedQuestion={this.state.selectedQuestion}
-                    />
-
-                    <Question 
-                        question={question}
-                        answers={answers}
-                        userChoose={userChoose}
-                        onRadioButtonChange={(event) => this.onRadioButtonChange(event)}
-                    />
-
-                    <Button 
-                        text="NEXT QUESTION"
-                        type="next"
-                        onClick={() => this.onNextClick()}
-                        selectedQuestion={this.state.selectedQuestion}
-                    />
-
-                    <Button 
-                        text="PREVIOUS QUESTION"
-                        type="previous"
-                        onClick={() => this.onPreviousClick()}
-                        selectedQuestion={this.state.selectedQuestion}
-                    />
-                </div>
-            )
+            if(this.state.displayResult){
+                console.log(this.state.data)
+                return(
+                    <div>
+                        <h1>Result:</h1>
+                        <h2>{this.state.data.reduce((accumulator, question) => accumulator + (question.answer === parseInt(question.userChoose) ? 10 : 0), 0)}</h2>
+                    </div>
+                )
+            }
+            else{
+                return (
+                    <div>
+                        <QuestionsNavbar 
+                            answers={this.state.data.map(question => question.userChoose)}
+                            onClick={(index) => this.onNavbarClick(index)}
+                            selectedQuestion={this.state.selectedQuestion}
+                        />
+    
+                        <Question 
+                            question={question}
+                            answers={answers}
+                            userChoose={userChoose}
+                            onRadioButtonChange={(event) => this.onRadioButtonChange(event)}
+                        />
+    
+                        <Button 
+                            text="NEXT QUESTION"
+                            type="next"
+                            onClick={() => this.onNextClick()}
+                            selectedQuestion={this.state.selectedQuestion}
+                            disabled={this.state.data.some(question => question.userChoose === -1)}
+                        />
+    
+                        <Button 
+                            text="PREVIOUS QUESTION"
+                            type="previous"
+                            onClick={() => this.onPreviousClick()}
+                            selectedQuestion={this.state.selectedQuestion}
+                        />
+                    </div>
+                )
+            }
         }
         else {
             return <h2>Loading...</h2>
